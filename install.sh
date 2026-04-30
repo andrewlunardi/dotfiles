c04a939d9fc7c389c1d195e34af95bceffc75ab4
andrewlu99 <andrewlunardi16@gmail.com>
2026-04-30 17:01:01 +0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Setting up dotfiles..."
+
+#Install git kalau belum ada
+which git || sudo pacman -S git --noconfirm
+
+#Clone dotfiles
+git clone --bare https://github.com/andrewlunardi/dotfiles.git $HOME/.dotfiles
+
+#Alias Sementara
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+#Checkout config
+dotfiles checkout 2>/dev/null || {
+	echo "Backing up existing configs..."
+	mkdir -p ~/.cconfig-backup
+	dotfiles checkout 2>&1 | grep "\s+\." | awk {'print $1'} | xargs -I() mv {} ~/.config-backup/{}
+	dotfiles checkout
+}
+
+dotfiles config --local status.showUntrackedFiles no
+
+echo "Done! Dotfiles installed."
