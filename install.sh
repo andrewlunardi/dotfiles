4f6438b649791c3c7d399e326ead4b45af0879b0
andrewlunardi <andrewlunardi16@gmail.com>
2026-05-18 17:26:01 +0700

    Update install script for dotfiles setup

--- install.sh
+++ install.sh
@@ -2,11 +2,37 @@
 
 echo "Setting up dotfiles..."
 
-# Install git kalau belum ada
-which git || sudo pacman -S git --noconfirm
+# Install dependencies dasar
+sudo pacman -S --noconfirm git zsh curl wget
+
+# Install Oh My Zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    echo "Oh My Zsh already installed, skipping..."
+fi
+
+# Install zsh plugins
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    echo "Installing zsh-autosuggestions..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    echo "Installing zsh-syntax-highlighting..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+fi
 
 # Clone dotfiles
-git clone --bare https://github.com/andrewlunardi/dotfiles.git $HOME/.dotfiles
+if [ ! -d "$HOME/.dotfiles" ]; then
+    echo "Cloning dotfiles..."
+    git clone --bare https://github.com/andrewlunardi/dotfiles.git $HOME/.dotfiles
+else
+    echo "Dotfiles repo already exists, skipping..."
+fi
 
 # Pakai function, bukan alias
 dotfiles() {
@@ -23,4 +49,11 @@ dotfiles checkout 2>/dev/null || {
 
 dotfiles config --local status.showUntrackedFiles no
 
+# Set zsh sebagai default shell
+if [ "$SHELL" != "$(which zsh)" ]; then
+    echo "Setting zsh as default shell..."
+    chsh -s $(which zsh)
+fi
+
 echo "Done! Dotfiles installed."
+echo "Please restart your terminal or run: exec zsh"
