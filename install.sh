a461eda856e6914223e554ee67a789aaabf1f40a
andrewlunardi <andrewlunardi16@gmail.com>
2026-05-18 16:11:33 +0700

    fix syntax typo

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 #Checkout config
 dotfiles checkout 2>/dev/null || {
 	echo "Backing up existing configs..."
-	mkdir -p ~/.cconfig-backup
-	dotfiles checkout 2>&1 | grep "\s+\." | awk {'print $1'} | xargs -I() mv {} ~/.config-backup/{}
+	mkdir -p ~/.config-backup
+	dotfiles checkout 2>&1 | grep "\s+\." | awk {'print $1'} | xargs -I{} mv{} ~/.config-backup/{}
 	dotfiles checkout
 }
 
