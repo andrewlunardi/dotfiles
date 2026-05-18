36232f9715246bf638301fb8c2cd558112f23168
andrewlunardi <andrewlunardi16@gmail.com>
2026-05-18 16:17:40 +0700

    big fix

--- install.sh
+++ install.sh
@@ -21,4 +21,29 @@ dotfiles checkout 2>/dev/null || {
 
 dotfiles config --local status.showUntrackedFiles no
 
+echo "Done! Dotfiles installed."#!/bin/bash
+
+echo "Setting up dotfiles..."
+
+# Install git kalau belum ada
+which git || sudo pacman -S git --noconfirm
+
+# Clone dotfiles
+git clone --bare https://github.com/andrewlunardi/dotfiles.git $HOME/.dotfiles
+
+# Pakai function, bukan alias
+dotfiles() {
+    /usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" "$@"
+}
+
+# Checkout config
+dotfiles checkout 2>/dev/null || {
+    echo "Backing up existing configs..."
+    mkdir -p ~/.config-backup
+    dotfiles checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv {} ~/.config-backup/{}
+    dotfiles checkout
+}
+
+dotfiles config --local status.showUntrackedFiles no
+
 echo "Done! Dotfiles installed."
